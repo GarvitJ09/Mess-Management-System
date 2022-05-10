8a17ecfceaeff1c9f08b75beda79a838ca6b31c7
GarvitJ09 <garvit992001@gmail.com>
2022-05-10 23:16:21 +0530

    version 1.0

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import router from "./routers/student.js";
+import routerMeal from "./routers/studentMealInfo.js";
+import dotenv from "dotenv";
+dotenv.config({ path: "./.env" });
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+app.use("/api/students", router);
+app.use("/api/meal", routerMeal);
+mongoose
+  .connect(process.env.DATABASE)
+  .then(() => {
+    app.listen(PORT);
+  })
+  .then(() => {
+    console.log("Conected to database and Listening to local host 5000");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
